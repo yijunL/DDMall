16d9e005674ba4cd2d6165f7477abe5779dc0757
yijunL <852131931@qq.com>
2019-12-12 14:42:35 +0800

    New automatic code generation function

---
+++ src/main/java/xmu/oomall/mapper/DefaultFreightMapper.java
@@ -0,0 +1,17 @@
+package xmu.oomall.mapper;
+
+import xmu.oomall.domain.DefaultFreight;
+
+public interface DefaultFreightMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(DefaultFreight record);
+
+    int insertSelective(DefaultFreight record);
+
+    DefaultFreight selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(DefaultFreight record);
+
+    int updateByPrimaryKey(DefaultFreight record);
+}
