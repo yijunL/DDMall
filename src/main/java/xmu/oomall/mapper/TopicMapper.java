51b1ede9f9601665910cc9acf07bcd28eb552179
yijunL <852131931@qq.com>
2019-12-12 11:30:15 +0800

    New automatic code generation function

---
+++ src/main/java/xmu/oomall/mapper/TopicMapper.java
@@ -0,0 +1,17 @@
+package xmu.oomall.mapper;
+
+import xmu.oomall.dao.Topic;
+
+public interface TopicMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Topic record);
+
+    int insertSelective(Topic record);
+
+    Topic selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Topic record);
+
+    int updateByPrimaryKey(Topic record);
+}
