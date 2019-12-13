3f3fe2ae0c6226eda585b6ff7fe4704f5ba562ba
Bexasusual <3046853673@qq.com>
2019-12-13 23:57:19 +0800

    Update FootprintServiceImpl.java

--- src/main/java/xmu/oomall/service/FootprintServiceImpl.java
+++ src/main/java/xmu/oomall/service/FootprintServiceImpl.java
@@ -3,29 +3,25 @@ package xmu.oomall.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
-import xmu.oomall.controller.vo.GoodsVo;
 import xmu.oomall.domain.*;
 import xmu.oomall.dao.*;
-import xmu.oomall.service.GoodsService;
-import xmu.oomall.util.ResponseUtil;
+import xmu.oomall.dao.FootprintItem;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class FootprintServiceImpl {
+public class FootprintServiceImpl implements FootprintService {
+    @Autowired
+    private  FootprintItem footprintDAO;
 
     /**
-     *获取足迹信息/list
+     * 获取足迹信息/list
      */
-    @GetMapping("/footprints")
-    public Object list(String userId, String goodsId,
-                       @RequestParam(defaultValue = "1") Integer page,
-                       @RequestParam(defaultValue = "10") Integer limit,
-                       @RequestParam(defaultValue = "add_time") String sort,
-                       @RequestParam(defaultValue = "desc") String order);
-//                       @Sort @RequestParam(defaultValue = "add_time") String sort,
-//                      @Order @RequestParam(defaultValue = "desc") String order;
+    @Override
+    public Object list(String userId, String goodsId, Integer page, Integer limit, String sort, String order) {
+        return null;
+    }
 
     /**
      * 删除用户足迹/delete
@@ -34,26 +30,29 @@ public class FootprintServiceImpl {
      * @param body   请求内容， { id: xxx }
      * @return 删除操作结果
      */
-    @DeleteMapping("{id}")
-    public Object delete(@LoginUser Integer userId, @RequestBody String body);
+    @Override
+    public Object delete(Integer userId, String body) {
+        return null;
+    }
 
     /**
      * 获取用户足迹列表/list
      *
-     * @param page 分页页数
+     * @param page  分页页数
      * @param limit 分页大小
      * @return 用户足迹列表
      */
-    @GetMapping("")
-    public Object list(@LoginUser Integer userId,
-                       @RequestParam(defaultValue = "1") Integer page,
-                       @RequestParam(defaultValue = "10") Integer limit);
+    public Object list(Integer userId, Integer page, Integer limit) {
+        return null;
+    }
 
     /**
-     *内部接口:添加足迹信息/add
-     *@param userId 用户ID
-     *@param body
+     * 内部接口:添加足迹信息/add
+     *
+     * @param userId 用户ID
+     * @param body
      */
-    @PostMapping("/footprints")
-    public Object add(@LoginUser Integer userId, @RequestBody Footprint footprint);
+    public Object add(Integer userId, xmu.oomall.domain.FootprintItem footprint) {
+        return null;
+    }
 }
