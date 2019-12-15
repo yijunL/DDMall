5abe04bb05fc1d6c6c10b14e67e0f9821e26e9b8
Lin-qianen <1149739343@qq.com>
2019-12-15 21:58:58 +0800

    add Comment

--- src/main/java/xmu/oomall/service/CommentService.java
+++ src/main/java/xmu/oomall/service/CommentService.java
@@ -23,4 +23,20 @@ public interface CommentService {
      */
     public List<Comment> getCommentsById(Integer id);
 
+    /**
+     * 用户在产品下发表评论
+     *
+     * @param commentPo
+     * @return 0：失败  1：成功
+     */
+    public int addComment (CommentPo commentPo);
+
+    /**
+     * 管理员删除评论
+     *
+     * @param id
+     * @return 0：失败 1：成功
+     */
+    public int deleteComment(Integer id);
+
 }
--- src/test/java/xmu/oomall/controller/CommentControllerTest.java
+++ src/test/java/xmu/oomall/controller/CommentControllerTest.java
@@ -17,5 +17,18 @@ public class CommentControllerTest {
     @Test
     public void commentControllerTest(){
           commentController.getCommentsById(1);
+        //44,55,"yukeyishishabi",0,4,77,null,null,0
+        CommentPo commentPo= new CommentPo();
+        commentPo.setBeDeleted(false);
+        commentPo.setContent("yukeyishishabi");
+        commentPo.setId(111);
+        commentPo.setProductId(222);
+        commentPo.setStar(4);
+        commentPo.setStatusCode(0);
+        commentPo.setUserId(333);
+
+          //commentController.addComment(commentPo);
+
+          System.out.println(commentController.deleteComment(111));
     }
 }
