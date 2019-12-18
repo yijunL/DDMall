94f18a3d24e1f3551ef7d44d9d8cfcb4c8299306
Administrator <648752148@qq.com>
2019-12-18 22:50:21 +0800

    新增address模块和collection模块

---
+++ Address/src/main/java/xmu/oomall/dao/AddressDao.java
@@ -0,0 +1,63 @@
+package xmu.oomall.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import xmu.oomall.domain.Address;
+import xmu.oomall.domain.AddressPo;
+import xmu.oomall.mapper.OomallAddressMapper;
+import xmu.oomall.util.Copyer;
+import xmu.oomall.util.PageCut;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public class AddressDao {
+
+    @Autowired
+    private OomallAddressMapper oomallAddressMapper;
+
+
+    public List<Address> getAddressList(Integer userId,Integer page, Integer limit)
+    {
+        return (List<Address>) PageCut.pageCut(oomallAddressMapper.findAllByBeDeletedLessThan(userId),page,limit);
+    }
+
+    public Address getAddress(Integer id){
+        AddressPo addressPo=oomallAddressMapper.findAllById(id);
+        return addresses(addressPo);
+    }
+
+    public AddressPo addAddress(AddressPo addressPo){
+        if(addressPo.getId()!=null)    //插入的po不能有id
+            return null;
+        addressPo.setGmtCreate(LocalDateTime.now());
+        oomallAddressMapper.insertSelective(addressPo);
+        return addressPo;
+    }
+
+    public AddressPo updateAddress(Integer id,AddressPo addressPo){
+        if(addressPo.getId()!=null)
+            return null;                        //id不允许有值，即不能修改id
+        addressPo.setGmtModified(LocalDateTime.now());
+        oomallAddressMapper.updateById(addressPo,id);
+        return addressPo;
+    }
+
+    public boolean deleteAddress(Integer id) {
+
+        AddressPo addressPo=new AddressPo();
+        addressPo.setGmtModified(LocalDateTime.now());
+        addressPo.setBeDeleted(true);
+        if(oomallAddressMapper.updateById(addressPo,id)==0)  //没有更新任何元素
+            return false;
+        else
+            return true;
+    }
+
+    private Address addresses(AddressPo addressPo){
+        Address address1 = new Address();
+        return Copyer.Copy(addressPo,address1)?address1:null;
+    }
+
+}
---
+++ Address/src/test/java/xmu/oomall/controller/AddressControllerTest.java
@@ -0,0 +1,31 @@
+package xmu.oomall.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import xmu.oomall.dao.AddressDao;
+import xmu.oomall.domain.AddressPo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+public class AddressControllerTest {
+    @Autowired
+    private AddressController addressController;
+    @Autowired
+    private AddressDao addressDao;
+
+
+    @Test
+    public void deleteAddressTest()
+    {
+        addressController.deleteAddress(1);
+        AddressPo addressPo=addressDao.getAddress(1);
+        assertEquals(addressPo.getBeDeleted(),true);
+    }
+
+
+
+}
