2597e7b829334890dd3b72e38594095d6e6e3137
Administrator <648752148@qq.com>
2019-12-18 12:59:35 +0800

    地址增查删 已测试

---
+++ src/main/java/xmu/oomall/service/AddressService.java
@@ -0,0 +1,25 @@
+package xmu.oomall.service;
+
+
+import org.springframework.stereotype.Service;
+import xmu.oomall.domain.Address;
+import xmu.oomall.domain.AddressPo;
+import xmu.oomall.domain.CollectItem;
+import xmu.oomall.domain.CollectItemPo;
+
+
+import java.util.List;
+
+
+@Service
+public interface AddressService {
+
+    public List<AddressPo> getAddressList(Integer page, Integer limit);
+
+    public Address addAddress(Address address);
+
+//    public CollectItemPo addCollection(CollectItemPo collectItemPo);
+//
+    public boolean deleteAddress (Integer id);
+
+}
