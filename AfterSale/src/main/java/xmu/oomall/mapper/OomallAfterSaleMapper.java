fe14e5f3363cecf7032da60cbf552292d93785d4
Bexasusual <3046853673@qq.com>
2019-12-19 22:33:43 +0800

    完成售后部分全部接口，待测试

--- AfterSale/src/main/java/xmu/oomall/mapper/OomallAfterSaleMapper.java
+++ AfterSale/src/main/java/xmu/oomall/mapper/OomallAfterSaleMapper.java
@@ -14,4 +14,10 @@ public interface OomallAfterSaleMapper {
 
     int deleteById(@Param("updatedGmtModified") LocalDateTime updatedGmtModified, @Param("id")Integer id);
 
+    int updateById(@Param("updatedGmtModified") LocalDateTime updatedGmtModified, @Param("id")Integer id, @Param("updated")AfterSaleService updated);
+
+    int updateByIdForAdmin(@Param("updatedGmtModified") LocalDateTime updatedGmtModified, @Param("id")Integer id, @Param("updated")AfterSaleService updated);
+
+    List<AfterSaleService> selectAllByUserId(@Param("userId")Integer userId); //用户查询自己售后列表与管理员按条件(userId)查询售后服务共用该方法
+
 }
