f7e5cb8506099a14f2307b6e01fa53ba50f4efe9
Bexasusual <3046853673@qq.com>
2019-12-20 20:30:51 +0800

    实现足迹服务

--- Footprint/src/main/java/xmu/oomall/OomallApplication.java
+++ Footprint/src/main/java/xmu/oomall/OomallApplication.java
@@ -3,10 +3,12 @@ package xmu.oomall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
 @MapperScan("xmu.oomall.mapper")
+@EnableDiscoveryClient
 public class OomallApplication {
 
     public static void main(String[] args) {
@@ -14,3 +16,4 @@ public class OomallApplication {
     }
 
 }
+
