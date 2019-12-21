9b1488c31a7683f8f58e4a1d0dfd75cd42252f9b
wwwwwwww0808 <wwwwwwww0808@163.com>
2019-12-21 14:49:30 +0800

    share modual

---
+++ Share/src/main/java/xmu/oomall/AddGoods.java
@@ -0,0 +1,12 @@
+package xmu.oomall;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import xmu.oomall.domain.Goods;
+
+@FeignClient(value = "goodsService")
+public interface AddGoods {
+
+    @PostMapping("/goods/{id}/isOnSale")
+    boolean isOnsale(Integer id);
+
+}
